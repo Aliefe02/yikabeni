13576ead369801d7fd43e87158fa1f56ad493c90
Ali EFE <78353929+Aliefe02@users.noreply.github.com>
2024-02-01 13:46:39 +0300

    Update settings.py

--- yikabeni/yikabeni/settings.py
+++ yikabeni/yikabeni/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
-        'PASSWORD': 'Dominikefe2002'
+        'PASSWORD': '[DATABASE_PASSWORD]'
     }
 }
 
